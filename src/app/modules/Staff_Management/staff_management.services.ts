1fdba8315c9e095bb3d54fd8f5f39349f2eaeea7
rana16468 <amsohelrana.me@gmail.com>
2026-04-22 05:41:24 +0600

    sklahdflsadf sadjfhjklf nasdfhaskjf aksdfjads

--- src/app/modules/Staff_Management/staff_management.services.ts
+++ src/app/modules/Staff_Management/staff_management.services.ts
@@ -1,18 +1,58 @@
+import httpStatus from "http-status";
+import config from "../../../config";
+import ApiError from "../../../errors/ApiErrors";
 import catchError from "../../../errors/catchError";
+import prisma from "../../../shared/prisma";
 import generateId from "../../../utils/generateId";
 import { TStaffManagement } from "./staff_management.interface";
-
+import bcrypt from 'bcrypt'
 
 const createStaffManagementIntoDb=async(userId: string, payload:TStaffManagement)=>{
 
 
      try{
 
-         const gId= await generateId(payload.role);
+        const isExistAccount=await prisma.staff.findFirst({where:{
+            email:payload.email
+        }});
 
-        return {
-            userId, payload, gId
-        }
+        if(isExistAccount){
+            throw new ApiError(httpStatus.NOT_EXTENDED, 'this account already exist')
+        };
+
+          const gId= await generateId(payload.role);
+          const hashedPassword = await bcrypt.hash(
+               payload.password,
+               Number(config.bcrypt_salt_rounds)
+             );
+
+              const result=await prisma.staff.create({
+                data:{
+                  email: payload.email,
+                  generateId: gId,
+                  password: hashedPassword,
+                  
+                  name: payload.name,
+                  role: payload.role,
+                  phoneNumber: payload.phoneNumber,
+                  subscriptionId: payload.subscriptionId
+
+                }
+              });
+
+              if(!result){
+                throw new ApiError(httpStatus.NOT_EXTENDED, 'issues by the ')
+              };
+
+              return {
+                success:true , 
+                message:" successfully create account "
+              }
+
+
+             
+
+        
 
      }
      catch(error){
